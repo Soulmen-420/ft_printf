d2dcbb48b62469ea82e53ccfed594be9b8623868
Soulmen <>
2020-06-30 21:13:34 +0200

    First ground work for the project

---
+++ ft_print_f/ft_printf.h
@@ -0,0 +1,12 @@
+#ifndef __FT_PRINTF_H__
+# define __FT_PRINTF_H__
+
+    #include "/Users/tjeerd/libft/ft_include.h"    
+
+    #include <unistd.h>
+    #include <stdlib.h>
+    #include <stdarg.h>
+
+
+
+#endif
