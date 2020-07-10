e758973828b49973122edc9b34416b2578e3c8b5
Soulmen <>
2020-07-10 14:41:12 +0200

    Extra info read me, no argument printin ft_rintf.c

--- README.md
+++ README.md
@@ -14,7 +14,32 @@ D) computing unsigned numbers in base 2 (%b), 8 (%o), 10 (%u) and 16 (%x).
 E) displaying signed numbers (%d %D %i).
 F) displaying pointer address (%p).
 
+The va_list Type
+
+The va_list type is an array containing a single element of one structure containing the necessary information to implement the va_arg macro. The C deﬁnition of va_list type is given in ﬁgure 3.34
+
+// Figure 3.34
+typedef struct {
+   unsigned int gp_offset;
+   unsigned int fp_offset;
+   void *overflow_arg_area;
+   void *reg_save_area;
+} va_list[1];
+
+The va_start Macro
+
+The va_start macro initializes the structure as follows:
+
+reg_save_area The element points to the start of the register save area.
+
+overﬂow_arg_area This pointer is used to fetch arguments passed on the stack. It is initialized with the address of the ﬁrst argument passed on the stack, if any, and then always updated to point to the start of the next argument on the stack.
+
+gp_offset The element holds the offset in bytes from reg_save_area to the place where the next available general purpose argument register is saved. In case all argument registers have been exhausted, it is set to the value 48 (6 ∗ 8).
+
+fp_offset The element holds the offset in bytes from reg_save_area to the place where the next available ﬂoating point argument register is saved. In case all argument registers have been exhausted, it is set to the value 304 (6 ∗ 8 + 16 ∗ 16).
 
 https://linux.die.net/man/3/printf
 
 https://android.googlesource.com/kernel/lk/+/master/lib/libc/printf.c
+
+https://en.cppreference.com/w/cpp/io/c/fprintf
--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -4,10 +4,14 @@ int     ft_format_finder(const char *restrict format, va_list arg)
 {
     char *format_array[] = {"%c", "%d", "%i", "%f", "%o", "%s", "%u", "%x", "%X", "%p"};
     int a = 0;
+    static int count = 0;
     while(format_array[a]){
         if(format[a] == *format_array[0]){
-            char buffer = va_arg(arg, int);
-            ft_printf_char(&buffer);
+            count++;
+            printf("%d\n", count);
+            int buffer = va_arg(arg, int);
+            printf("buffer afther va_arg: %c\n", buffer);
+            ft_printf_char(buffer);
             printf("format finder format :%s\n", format_array[a]);
             return(1);
         }
@@ -30,15 +34,16 @@ int     ft_printf(const char *restrict format, ...)
             if((!*format) || (ft_format_finder(format, arg) <= 0)){
                 break;
             } else {
-                printf("\nformat in ascii = %d\n\n", ft_format_finder(format, arg));
+                printf("\nformat finder 1 succes else error = %d\n\n", ft_format_finder(format, arg));
                 break;
             }
         } else if(count == arg_len){
-            printf("max count: %s\n", format);
+            printf("Max count, no percent found, string is: %s ", format);
             return(0);
         }
         count++;
     }
+    printf("va_end reached");
     va_end(arg);
     return(count);
 }
@@ -46,8 +51,8 @@ int     ft_printf(const char *restrict format, ...)
 
 int     main(void)
 {
-   ft_printf("%c", "h");
-    printf("main count = %d\n", ft_printf("hello\n", "bye\n"));
-//    ft_printf("Hello\n" "bye\n"); 
+    ft_printf("%c", "h");
+//    printf("main count = %d\n", ft_printf("hello\n", "bye\n"));
+//    ft_printf("Hello\n" "bye\n" "goodjob\n"); 
     return(0);
 }
